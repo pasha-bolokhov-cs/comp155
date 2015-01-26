d9543a20ac2ac790e04bf722505939b8f7367082
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-01-26 10:58:29 -0800

    Add 'assign8'

---
+++ assign8/assign8.sql
@@ -0,0 +1,52 @@
+-- Run command: @H:\comp155\labsol\Lab8_SolCmd
+set echo on
+SPOOL H:\comp155\labsol\Lab8_Sol.txt
+SET PAGESIZE 50
+
+/*********************************
+ Assignment - Lab 8 Solution
+ By <give your name>
+ Lab Section: 1A or 1B or 2A or 2B
+ Time spent working on this lab: xx.x hrs
+ **********************************/
+ 
+/*** 1 ***/
+/* Create a query to display the employee numbers, last names 
+and salaries of all employees who earn more than the average 
+salary and whose last_name begins with a 'G' or 'P' (be sure 
+to handle case). Sort the results in descending order of salary. */
+
+/* Place SQL statement here */
+
+
+/*** 2 ***/
+/* Write a query that will display the last name and salary of every 
+employee who reports to Gerald Cambrault. (There is more than one 
+Cambrault in the database so you must be careful to check the first 
+name too.) Be sure to handle case. */
+
+/* Place SQL statement here */
+
+
+/*** 3 ***/
+/* How many people work in the same department as Gerald Cambrault? 
+Write a query to count the number employees in Gerald's department. 
+Exclude Gerald from your count. (Remember, there is more than one 
+Cambrault in the database so be sure to check the first name too.) */
+
+
+/* Place SQL statement here */
+
+
+
+/*** 4 ***/
+/* Write a query to display the department IDs, employee IDs, last names, 
+and salaries of all employees who earn more than twice ( 2 times) the 
+average salary for the company, and who work in a department with any 
+employee with a "u" anywhere in their last name. (Remember the "u" could 
+be at the beginning of the name too.) Order your output by employee IDs. */
+
+/* Place SQL statement here */
+
+
+SPOOL OFF
