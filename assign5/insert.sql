6c4e2b7d079b99e26feb0384006ec3e3f3f74c61
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-01-26 16:58:14 -0800

    Add 'insert.sql'

---
+++ assign5/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO employees
+(employee_id, first_name, last_name, email, hire_date, job_id, salary)
+VALUES
+(9989, 'bill', 'rIm', 'rim@home.com', sysdate, 'PU_CLERK', 1500.25);
+
+INSERT INTO employees
+(employee_id, first_name, last_name, email, hire_date, job_id, salary)
+VALUES
+(9988, 'LARRY', 'rIMMER', 'rimmer@myhome.com', sysdate, 'PU_CLERK', 1600.25);
