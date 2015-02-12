69c9dce7397c4215a609e01c11ecd12af0b734a6
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-12 11:13:13 -0800

    Add 'assign8.sql' and 'assign8.out'

--- assign8/assign8.sql
+++ assign8/assign8.sql
@@ -1,13 +1,13 @@
--- Run command: @H:\comp155\labsol\Lab8_SolCmd
+-- Run command: @assign8
 set echo on
-SPOOL H:\comp155\labsol\Lab8_Sol.txt
+SPOOL assign8.out
 SET PAGESIZE 50
 
 /*********************************
  Assignment - Lab 8 Solution
- By <give your name>
- Lab Section: 1A or 1B or 2A or 2B
- Time spent working on this lab: xx.x hrs
+ By Pasha Bolokhov
+ Lab Section: 1B
+ Time spent working on this lab: 1 hrs
  **********************************/
  
 /*** 1 ***/
@@ -15,8 +15,13 @@ SET PAGESIZE 50
 and salaries of all employees who earn more than the average 
 salary and whose last_name begins with a 'G' or 'P' (be sure 
 to handle case). Sort the results in descending order of salary. */
-
-/* Place SQL statement here */
+SELECT last_name AS "Last Name", salary AS "Salary"
+       FROM employees
+       WHERE salary > 
+       	     (SELECT AVG(salary) FROM employees)
+       AND (UPPER(last_name) LIKE 'G%'
+       	    OR UPPER(last_name) LIKE 'P%')
+       ORDER BY salary DESC;
 
 
 /*** 2 ***/
@@ -24,8 +29,13 @@ to handle case). Sort the results in descending order of salary. */
 employee who reports to Gerald Cambrault. (There is more than one 
 Cambrault in the database so you must be careful to check the first 
 name too.) Be sure to handle case. */
-
-/* Place SQL statement here */
+SELECT last_name AS "Last Name", salary AS "Salary"
+       FROM employees
+       WHERE manager_id = 
+       	     (SELECT employee_id
+	      FROM employees
+	      WHERE UPPER(first_name) = 'GERALD'
+	      AND UPPER(last_name) = 'CAMBRAULT');
 
 
 /*** 3 ***/
@@ -33,9 +43,15 @@ name too.) Be sure to handle case. */
 Write a query to count the number employees in Gerald's department. 
 Exclude Gerald from your count. (Remember, there is more than one 
 Cambrault in the database so be sure to check the first name too.) */
-
-
-/* Place SQL statement here */
+SELECT COUNT(last_name)
+       FROM employees
+       WHERE department_id =
+       	     (SELECT department_id
+	      FROM employees
+	      WHERE UPPER(first_name) = 'GERALD'
+	      AND UPPER(last_name) = 'CAMBRAULT')
+       AND NOT (UPPER(first_name) = 'GERALD'
+	      AND UPPER(last_name) = 'CAMBRAULT');
 
 
 
@@ -45,8 +61,15 @@ and salaries of all employees who earn more than twice ( 2 times) the
 average salary for the company, and who work in a department with any 
 employee with a "u" anywhere in their last name. (Remember the "u" could 
 be at the beginning of the name too.) Order your output by employee IDs. */
-
-/* Place SQL statement here */
-
+SELECT department_id AS "Dept No", employee_id AS "Emp No",
+       last_name AS "Last Name", salary AS "Salary"
+       FROM employees
+       WHERE salary >
+       	     (SELECT 2 * AVG(salary) FROM employees)
+       AND department_id IN
+       	   (SELECT department_id
+	    FROM employees
+	    WHERE UPPER(last_name) LIKE '%U%')
+       ORDER BY employee_id;
 
 SPOOL OFF
