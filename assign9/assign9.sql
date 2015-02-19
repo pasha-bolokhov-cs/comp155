f979731048ce233988f4a64eb14e77d241ea5832
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-19 02:42:57 -0800

    Edit more

--- assign9/assign9.sql
+++ assign9/assign9.sql
@@ -7,6 +7,7 @@
 set echo on
 SPOOL assign9.out
 set pagesize 50
+set linesize 100
 
 /*********************************
  Assignment - Lab 9 Solution
@@ -15,84 +16,105 @@ set pagesize 50
  **********************************/
  
 /*** 1 Insert ***/
+INSERT INTO customers (customer_id, first_name, last_name, dob, phone)
+       VALUES (50, 'Grace', 'Hopper', TO_DATE('December 9, 1906', 'Month DD, YYYY'), '800-555-1250');
+INSERT INTO customers (customer_id, first_name, last_name, dob, phone)
+       VALUES (51, 'Steve', 'Jobs', TO_DATE('February 24, 1955', 'Month DD, YYYY'), '800-555-1251');
+INSERT INTO customers (customer_id, first_name, last_name, dob, phone)
+       VALUES (52, 'Larry', 'Ellison', TO_DATE('August 17, 1944', 'Month DD, YYYY'), '800-555-1252');
+INSERT INTO customers (customer_id, first_name, last_name, dob, phone)
+       VALUES (53, 'Tom', 'Thomson', TO_DATE('August 4, 1877', 'Month DD, YYYY'), NULL);
 
-REM SQL command here
 
 /*** 2 Confirm ***/
+SELECT * FROM customers
+       ORDER BY customer_id;
 
-REM SQL command here
 
 /*** 3 Correct last name ***/
+UPDATE customers 
+       SET last_name = 'Thomas'
+       WHERE customer_id = 53;
 
-REM SQL command here
 
 /*** 4 Remove Jobs ***/
+DELETE FROM customers
+       WHERE customer_id = 51;
 
-REM SQL command here
 
 /*** 5 Update customer_ids ***/
+UPDATE customers
+       SET customer_id = customer_id + 100;
 
-REM SQL command here
 
 /*** 6 Confirm changes ***/
+SELECT * FROM customers
+       ORDER BY customer_id;
 
-REM SQL command here
 
 /*** 7 Create Savepoint ***/
+SAVEPOINT CUST_SAVEP;
 
-REM SQL command here
 
 /*** 8 Add yourself ***/
+INSERT INTO customers
+       VALUES (200, 'Pasha', 'Bolokhov', TO_DATE('January 31, 1940', 'Month DD, YYYY'), '778-356-5612');
 
-REM SQL command here
 
 /*** 9 Confirm changes ***/
+SELECT * FROM customers
+       ORDER BY customer_id;
 
-REM SQL command here
 
 /*** 10 Rollback to savepoint ***/
+ROLLBACK TO CUST_SAVEP;
 
-REM SQL command here
 
 /*** 11 List contents ***/
+SELECT * FROM customers
+       ORDER BY customer_id;
 
-REM SQL command here
 
 /*** 12 Rollback ***/
+ROLLBACK;
 
-REM SQL command here
 
 /*** 13 List contents ***/
+SELECT * FROM customers
+       ORDER BY customer_id;
 
-REM SQL command here
 
 /*** 14 Re-insert yourself ***/
+INSERT INTO customers
+       VALUES (200, 'Pasha', 'Bolokhov', TO_DATE('January 31, 1940', 'Month DD, YYYY'), '778-356-5612');
 
-REM SQL command here
 
 /*** 15 Commit ***/
-
-REM SQL command here
+COMMIT;
 
 /*** 16 List contents ***/
-
-REM SQL command here
+SELECT * FROM customers
+       ORDER BY customer_id;
 
 /*** 17 Rollback to savepoint & explanation ***/
+ROLLBACK TO CUST_SAVEP;
+-- CUST_SAVEP was erased already at step 12, 
+-- not to mention the COMMIT if step 12 wasn't there
 
-REM SQL command here
 
 /*** 18 List contents ***/
+SELECT * FROM customers
+       ORDER BY customer_id;
 
-REM SQL command here
 
 /*** 19 Select all records ***/
+SELECT customer_id, first_name, last_name, TO_CHAR(dob, 'fmMonth DD, YYYY'), phone
+       FROM customers;
 
-REM SQL command here
 
 /*** 20 Spool off ***/
+SPOOL OFF
 
-REM SQL command here
 
 /*** Drop the tables ***/
 
