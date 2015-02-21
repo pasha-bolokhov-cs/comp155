9d6e453549f23bf294897ef36108e9490f59c73e
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-20 19:23:58 -0800

    Add 'assign10' and 'assign11'

---
+++ assign11/assign11.sql
@@ -0,0 +1,81 @@
+-- Run command: @H:\comp155\labsol\Lab11_SolCmd
+
+set echo on
+SPOOL H:\comp155\labsol\Lab11_Sol.txt
+set pagesize 15
+set linesize 80
+
+/*********************************
+ Assignment - Lab 11 Solution
+ By <give name>
+ Lab Section: ??
+ Time Used Completing Lab: xx.x hours
+ **********************************/
+ 
+/*** 1 Create MY_STUDENT ***/
+ 
+REM SQL command here
+ 
+/*** 2 Describe ***/
+
+REM SQL command here
+
+/*** 3 Modify MY_STUDENT ***/
+
+REM SQL command here
+
+/*** 4 Describe ***/
+
+REM SQL command here
+
+/*** 5 Insert your name ***/
+
+REM SQL command here
+
+/*** 6 List contents ***/
+
+REM SQL command here
+
+/*** 7 Add comment ***/
+ 
+REM SQL command here
+
+/*** 8 Confirm comment for MY_STUDENT ***/
+
+REM SQL command here
+
+/*** 9 Create table My_Student ***/
+
+REM SQL command here
+
+/*** 10 Describe ***/
+
+REM SQL command here
+
+/*** 11 Select from data dictionary ***/
+
+REM SQL command here
+   
+/*** 12 Drop MY_STUDENT ***/
+
+REM SQL command here
+
+/*** 13 Rename ***/
+
+REM SQL command here
+
+/*** 14 Select from data dictionary ***/
+
+REM SQL command here
+
+/*** 15 Drop MY_STUDENT ***/
+
+REM SQL command here
+
+/*** 16 Select from data dictionary ***/
+
+REM SQL command here
+
+/*** 17 ***/
+
+SPOOL OFF
