7c99158eebc09a834f726a126a3be9635a0054a6
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-02-25 11:58:27 -0800

    Edit more

--- assign11/assign11.sql
+++ assign11/assign11.sql
@@ -1,48 +1,54 @@
--- Run command: @H:\comp155\labsol\Lab11_SolCmd
+-- Run command: @assign11
 
 set echo on
-SPOOL H:\comp155\labsol\Lab11_Sol.txt
+SPOOL assign11.out
 set pagesize 15
 set linesize 80
 
 /*********************************
  Assignment - Lab 11 Solution
- By <give name>
- Lab Section: ??
+ By Pasha Bolokhov
+ Lab Section: 1B
  Time Used Completing Lab: xx.x hours
  **********************************/
  
 /*** 1 Create MY_STUDENT ***/
- 
-REM SQL command here
+CREATE TABLE MY_STUDENT (
+       mystudent_id	NUMBER(5) PRIMARY KEY,
+       mylname		VARCHAR2(25),
+       mystartdate	DATE DEFAULT SYSDATE
+);
+
  
 /*** 2 Describe ***/
+DESCRIBE MY_STUDENT
 
-REM SQL command here
 
 /*** 3 Modify MY_STUDENT ***/
+ALTER TABLE MY_STUDENT 
+      MODIFY mylname VARCHAR2(50);
 
-REM SQL command here
 
 /*** 4 Describe ***/
+DESC MY_STUDENT
 
-REM SQL command here
 
 /*** 5 Insert your name ***/
+INSERT INTO MY_STUDENT (mystudent_id, mylname)
+       VALUES (5515, 'Bjoerndalen');
 
-REM SQL command here
 
 /*** 6 List contents ***/
-
-REM SQL command here
+SELECT * FROM MY_STUDENT;
 
 /*** 7 Add comment ***/
+COMMENT ON TABLE MY_STUDENT
+	IS 'Assignment 11 Table';
  
-REM SQL command here
 
 /*** 8 Confirm comment for MY_STUDENT ***/
-
-REM SQL command here
+SELECT TABLE_NAME, COMMENTS FROM USER_TAB_COMMENTS
+       WHERE TABLE_NAME = 'MY_STUDENT';
 
 /*** 9 Create table My_Student ***/
 
@@ -69,8 +75,8 @@ REM SQL command here
 REM SQL command here
 
 /*** 15 Drop MY_STUDENT ***/
+DROP TABLE my_student PURGE;
 
-REM SQL command here
 
 /*** 16 Select from data dictionary ***/
 
