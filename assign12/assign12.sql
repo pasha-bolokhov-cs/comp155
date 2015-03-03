1bc059bd7791fbb48c4863ac220c9c207719e974
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-03-02 23:25:00 -0800

    Add 'assign12'

---
+++ assign12/assign12.sql
@@ -0,0 +1,83 @@
+-- Run command: @H:\comp155\labsol\Lab12_SolCmd
+
+set echo on
+SPOOL H:\comp155\labsol\Lab12_Sol.txt
+SET PAGESIZE 50
+
+/*********************************
+ Assignment - Lab 12 Solution
+ By <give name>
+ Lab Section: ??
+**********************************/
+
+/*** Step 1 Create NEW_STUDENTS ***/
+
+REM SQL command here
+ 
+/*** Step 2 Add columns ***/ 
+
+REM SQL command here
+
+/*** Step 3 Confirm additions ***/     
+
+REM SQL command here
+
+/*** Step 4 Confirm constraints for NEW_STUDENTS ***/
+
+REM SQL command here
+
+/*** Step 5 Create PROGRAMMES ***/
+
+REM SQL command here
+
+/*** Step 6 Run insert script ***/
+
+REM SQL command here
+
+/*** Step 7 SELECT from created tables ***/
+
+REM SQL command here
+ 
+/*** Step 8 Alter PROGRAMMES ***/
+
+REM SQL command here
+
+/*** Step 9 Alter NEW_STUDENTS ***/
+
+REM SQL command here
+
+/*** Step 10 Add foreign key constraint ***/
+
+REM SQL command here
+
+/*** Step 11 Confirm constraints for new tables ***/
+
+REM SQL command here
+
+/*** Step 12 Insert into NEW_STUDENTS ***/
+
+REM SQL command here
+
+/*** Step 13 Explain error ***/
+
+REM You should get an error when you insert the record in Step 12. Why? 
+
+/*** Step 14 Add date constraint ***/
+
+REM SQL command here
+
+/*** Step 15 Test date constraint ***/
+
+REM SQL command here
+
+/*** Step 16 List constraints for new tables ***/
+
+REM SQL command here
+
+/*** Step 17 Clean up schema ***/
+
+REM SQL commands here
+
+/*** Step 18 End ***/
+
+SPOOL OFF
---
+++ assign12/inserts.sql
@@ -0,0 +1,9 @@
+INSERT INTO new_students
+(temp_id,prog_id,fname,lname,reg_date, created_by)
+VALUES
+(9516, 'comp','Ted','Codd',sysdate,user);
+
+INSERT INTO programmes
+(prog_id, prog_desc)
+VALUES
+('comp','Computer Technology');
