48968ca952535a2e3b2a85fbcf88b69aac8bb390
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-03-04 12:09:28 -0800

    Edit more

--- assign13/assign13.sql
+++ assign13/assign13.sql
@@ -6,48 +6,56 @@ SET PAGESIZE 50
 
 /*********************************
  Assignment - Lab 13 Solution
- By <give name>
- Lab Section: ??
- Approx. Time Spent: x.xx hrs
+ By Pasha Bolokhov
+ Lab Section: 1B
+ Approx. Time Spent: 1 hr
  **********************************/
 
 /*** Step 1 Create EMPLOYEES_VW ***/
-
-REM SQL command here
+CREATE OR REPLACE VIEW EMPLOYEES_VW
+       AS SELECT employee_id, first_name || ' ' || last_name AS "EMPNAME", department_id
+       FROM employees;
 
 /*** Step 2 Display for Dept 90 ***/
-
-REM SQL command here
+SELECT * FROM EMPLOYEES_VW
+       WHERE department_id = 90;
 
 /*** Step 3 List Views from Dictionary ***/
-
-REM SQL command here
+SELECT VIEW_NAME, TEXT
+       FROM USER_VIEWS
+       WHERE VIEW_NAME = 'EMPLOYEES_VW';
 
 /*** Step 4 Create DEPT50 view ***/
-
-REM SQL command here
+CREATE OR REPLACE VIEW DEPT50
+       (EMPNO, EMPLOYEE, DEPTNO)
+       AS SELECT employee_id, last_name, department_id
+       	  FROM employees
+	  WHERE department_id = 50
+	  WITH CHECK OPTION CONSTRAINT DEPT_NO_C;
 
 /*** Step 5 Structure & Contents of DEPT50 ***/
-
-REM SQL command here (structure)
-REM SQL command here (contents)
+DESCRIBE DEPT50
+SELECT * FROM DEPT50
+       WHERE ROWNUM <= 10;
 
 /*** Step 6 Reassign Matos ***/
-
-REM SQL command here
+UPDATE DEPT50
+       SET DEPTNO = 80
+       WHERE UPPER(EMPLOYEE) = 'MATOS';
 
 /*** Step 7 Why did you get an error? ***/
-
-REM Explain the error here
+PROMPT The error occurred because of the constraint DEPT_NO_C
 
 /*** Step 8 List views from USER_VIEWS ***/
+SELECT VIEW_NAME, TEXT
+       FROM USER_VIEWS
+       WHERE VIEW_NAME = 'EMPLOYEES_VW'
+       OR    VIEW_NAME = 'DEPT50';
 
-REM SQL command here
-      
 /*** Step 9 Drop the new views ***/
-
-REM SQL command here
+DROP VIEW EMPLOYEES_VW;
+DROP VIEW DEPT50;
 
 /*** Step 10 ***/
-
 SPOOL OFF
+
