b79bb0e0a5e7032bf5df976be67b155e8847c81c
Pasha Bolokhov <pasha.bolokhov@gmail.com>
2015-03-07 20:45:21 -0800

    Edit more

--- assign14/assign14.sql
+++ assign14/assign14.sql
@@ -3,32 +3,42 @@
 set echo on
 SPOOL assign14.out
 SET PAGESIZE 50
+SET LINESIZE 100
 
 /*********************************
  Assignment - Lab 14 Solution
- By <Give name>
- Lab Section: ??
+ By Pasha Bolokhov
+ Lab Section: 1B
  **********************************/
  
 /*** Step 1 Run script to create table & insert data ***/
+@create-textbook.sql
 
-REM SQL command here
 
 /*** Step 2 Create sequence ***/
+CREATE SEQUENCE	   textbook_seq
+       START WITH  22
+       NOCACHE;
 
-REM SQL command here
 
 /*** Step 3 Update with sequence ***/
+UPDATE MYTEXTBOOKTABLE
+       SET TEXT_ID = textbook_seq.NEXTVAL
+       WHERE TEXT_NAME = 'All computers';
+UPDATE MYTEXTBOOKTABLE
+       SET TEXT_ID = textbook_seq.NEXTVAL
+       WHERE TEXT_NAME = 'No Homework!';
 
-REM SQL command here
 
 /*** Step 4 Display details about textbook_seq ***/
+SELECT SEQUENCE_NAME, MAX_VALUE, INCREMENT_BY, LAST_NUMBER
+       FROM USER_SEQUENCES
+       WHERE SEQUENCE_NAME = 'TEXTBOOK_SEQ';
 
-REM SQL command here
 
 /*** Step 5 Make text_id the primary key ***/
+ALTER TABLE MYTEXTBOOKTABLE ADD CONSTRAINT text_id_pk PRIMARY KEY (TEXT_ID);
 
-REM SQL command here
 
 /*** Step 6 Insert values ***/
 
@@ -59,8 +69,8 @@ REM SQL command here
 REM SQL command here
       
 /*** Step 13 Drop schema objects ***/
-
-REM SQL command here
+DROP SEQUENCE textbook_seq;
+DROP TABLE MYTEXTBOOKTABLE PURGE;
 
 /*** Step 14 End ***/
 
